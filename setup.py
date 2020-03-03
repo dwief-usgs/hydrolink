c3822622324f98d47eceb7573a64f50e5872b464
dwief <dwieferich@usgs.gov>
2020-03-03 12:02:20 -0700

    add bumpversion

--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dwief-usgs/hydrolink',
-    version='0.1.0',
+    version='0.0.2',
     zip_safe=False,
 )
