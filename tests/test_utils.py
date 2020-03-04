b8ebe44af96b2e52646a82ad58bbe3b98e19e962
dwief <dwieferich@usgs.gov>
2020-03-04 15:32:17 -0700

    test_build_meas_line crs4269 only

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,7 +13,6 @@ def test_clean_water_name():
     -----------
     Test function utils.clean_water_name.  Ensure abbreviations are being correctly addressed.
     '''
-
     assert utils.clean_water_name('Grand st.') == 'grand stream'
     assert utils.clean_water_name('Grand St') == 'grand stream'
     assert utils.clean_water_name('grand str ') == 'grand stream'
@@ -39,12 +38,18 @@ def test_build_meas_line():
     Ensure distance between two points is being measured correctly.
     Line lengths were measured in arcmap.  Accounts for 1% rounding error.
     ''' 
-    crs_options = [{'crs': {'init':'epsg:4269'}, 'length_m': 2381.955938},{'crs': {'init':'epsg:4269'}, 'length_m': 2381.955938 }]
+    #Input CRS which all data are transformed too
+    crs = {'init':'epsg:4269'}
+    
+    #using shapely create 2 points to allow for measuring of connecting line
     point1 = Point(-72.522365, 41.485054)  
     point2 = Point(-72.529494, 41.464437)
-    for x in crs_options:
-        crs = x['crs']
-        val = x['length_m']
-        len = utils.build_meas_line(point1, point2, crs=crs)
-        assert  val-(val*0.01) <= len <= val+(val*0.01)
+
+    #test if the build_meas_line measurement is within 1% rounding error of arcmap measurement
+
+    arcmap_length_m = 2381.955938
+    arcmap_length_m_lt1 = arcmap_length_m - (arcmap_length_m*0.01)
+    arcmap_length_m_gt1 = arcmap_length_m + (arcmap_length_m*0.01)
+    len = utils.build_meas_line(point1, point2, crs=crs)
+    assert  arcmap_length_m_lt1 <= len <= arcmap_length_m_gt1 
 
