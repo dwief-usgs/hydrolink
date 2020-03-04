d9c186911c6df2837e118b907432ac794fc5f55d
dwief <dwieferich@usgs.gov>
2020-03-04 08:23:42 -0700

    hydrolinker update flow info, bumpversion

--- hydrolinker.py
+++ hydrolinker.py
@@ -6,83 +6,71 @@ import click
 
 
 @click.command()
-@click.option('--input_file', default='test-data/test.csv', help='Enter file name, including extension (only accepts .csv and .shp)')
-@click.option('--latitude_field', default='y', help='Enter field name for latitude, note this is case sensitive')
-@click.option('--longitude_field', default='x', help='Enter field name for longitude, note this is case sensitive')
-@click.option('--stream_name_field', default='stream', help='Enter field name for stream name, if none type None, note this is case sensitive')
-@click.option('--identifier_field', default='id', help='Enter field name for identifier, note this is case sensitive')
-@click.option('--crs', default=4269, help='Enter crs number, recommended to use NAD83 represented by 4269')
-def get_input( input_file, latitude_field, longitude_field, stream_name_field, identifier_field, crs):
+@click.option('--input_file', required=True,  default='test-data/test.csv', help='Enter file name, including extension (only accepts .csv and .shp)')
+@click.option('--latitude_field', required=True, default='y', help='Enter field name for latitude, note this is case sensitive')
+@click.option('--longitude_field', required=True, default='x', help='Enter field name for longitude, note this is case sensitive')
+@click.option('--stream_name_field', required=True, default='stream', help='Enter field name for stream name, if none type None, note this is case sensitive')
+@click.option('--identifier_field', required=True, default='id', help='Enter field name for identifier, note this is case sensitive')
+@click.option('--crs', required=True, default=4269, help='Enter crs number, recommended to use NAD83 represented by 4269')
+def handle_data(input_file, latitude_field, longitude_field, stream_name_field, identifier_field, crs):
     """
-    A program that helps you do things. 
+    a program to hydrolink point data to the nhd high resolution. 
+    
+    examples:
+    
+    python hydrolinker.py -> runs with defaults
+    python hydrolinker.py --input_file=file_name.csv  -> runs with specified file name and default other values
+
+    output: 
+    writes hydrolinked (hydro addressed information to csv file)
     """
+
     click.echo('Thank you, processing now...')
+
     in_data = {"file":input_file,"lat":latitude_field,"lon":longitude_field,"stream_name":stream_name_field,"id":identifier_field}
-    return in_data, crs
 
-
-def import_file(in_data, crs):
-    '''
-    Description: Imports CSV or SHP files into a Pandas dataframe. Makes sure that all fields 
-    are in the file and converts the file into a dataframe with a standard set of field names.
-    
-    Input
-    ------------
-    in_data: dictionary with values for file, lat, lon, stream_name, id
-    
-    Output
-    ------------
-    Pandas Dataframe
-    '''
-    #Check to see if the file is a CSV file
     if in_data['file'].endswith('.csv'):
-        print ('reading csv file' +'\n')
+        click.echo('reading csv file')
         try:
             df = pd.read_csv(in_data['file'],encoding='iso-8859-1')
         except KeyError:
-            print ('file did not properly import, verify file name and rerun')
-            #It would be nice here to reask for inputFileName and then restart at try statement
-    
+            click.echo('csv file did not properly import, verify file name and rerun')
+
     #If input file is not a CSV check to see if it is a shapefile
     elif in_data['file'].endswith('.shp'):
-        print('\n' + 'reading shapefile' + '\n')
+        click.echo('reading shapefile')
         try:
             df = gpd.GeoDataFrame.from_file(in_data['file'])
         except KeyError:
-            print ('file did not properly import, verify file name and rerun')
-            #It would be nice here to reask for inputFileName and then restart at try statement
-    
+            click.echo('shapefile did not properly import, verify file name and rerun')
+
     #If input file is not a CSV or shapefile tell the user that the file type is not excepted
     else:
-        print('File type not currently accepted. Please try .csv or .shp')
-    
-    
+       click.echo('File type not currently accepted. Please try .csv or .shp')
 
-    if in_data['lat'] in df and in_data['lon'] in df and in_data['id'] in df:
-        if in_data['stream_name'] is not None and in_data['stream_name'] in df:
-            df = df[[in_data['id'], in_data['lat'], in_data['lon'], in_data['stream_name']]].copy()
-            df = df.rename(columns={in_data['id']: 'id', in_data['lat']: 'lat', in_data['lon']: 'lon', in_data['stream_name']: 'stream' })
-            df['crs'] = int(crs)
-            return df
-        else:
-            df = df[[in_data['id'], in_data['lat'], in_data['lon']]].copy()
-            df = df.rename(columns={in_data['id']: 'id', in_data['lat']: 'lat', in_data['lon']: 'lon'})
-            df['stream'] = None
-            df['crs'] = int(crs)
-            return df
+
+    if in_data['lat'] in df and in_data['lon'] in df and in_data['id'] in df and in_data['stream_name'] in df:
+        df = df.rename(columns={in_data['id']: 'id', in_data['lat']: 'lat', in_data['lon']: 'lon', in_data['stream_name']: 'stream' })
+        df['crs'] = int(crs)
+
+    # if in_data['lat'] in df and in_data['lon'] in df and in_data['id'] in df:
+    #     if in_data['stream_name'] is not None and in_data['stream_name'] in df:
+    #         df = df[[in_data['id'], in_data['lat'], in_data['lon'], in_data['stream_name']]].copy()
+    #         df = df.rename(columns={in_data['id']: 'id', in_data['lat']: 'lat', in_data['lon']: 'lon', in_data['stream_name']: 'stream' })
+    #         df['crs'] = int(crs)
+
+    #     else:
+    #         df = df[[in_data['id'], in_data['lat'], in_data['lon']]].copy()
+    #         df = df.rename(columns={in_data['id']: 'id', in_data['lat']: 'lat', in_data['lon']: 'lon'})
+    #         df['stream'] = None
+            
+
     else: 
-        print ('verify field names and rerun')
+        click.echo('verify field names and rerun')
 
-
-if __name__ == '__main__':
-
-    #input_data, crs = get_user_input()
-    input_data, crs = get_input()
-
-    df = import_file(input_data, crs)
 
     for row in df.itertuples():
-        
+
         #initiate a Python object
         hydrolink = nhd_hr.HighResPoint(row.id, float(row.lat), float(row.lon), water_name=str(row.stream), input_crs=int(row.crs))
 
@@ -94,7 +82,7 @@ if __name__ == '__main__':
 
         #stream name match for each of the reaches
         hydrolink.water_name_match()
-    
+
         #Return reach most likely associated with point
         hydrolink.select_best_reach()
 
@@ -105,6 +93,12 @@ if __name__ == '__main__':
         #hydrolink.write_reach_options(outfile_name='test-data/test_hr_hydrolink_reach_output.csv')
 
         #Creates a CSV file with one reach per point.
-        in_file = input_data['file']  
+        in_file = in_data['file'][:-4]  #remove .csv or .shp
         output_file = f'{in_file}_output.csv'
         hydrolink.write_best(outfile_name= output_file)
+    
+    click.echo('Output exported to %s' % output_file)
+
+
+if __name__ == '__main__':
+    handle_data()
--- setup.py
+++ setup.py
@@ -43,6 +43,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dwief-usgs/hydrolink',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
 )
